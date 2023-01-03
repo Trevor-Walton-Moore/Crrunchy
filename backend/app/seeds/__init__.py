7248f8ec6cad90c772b3646091bbea675969e812
Trevor WM <Trevormoore.2016@gmail.com>
2023-01-03 08:58:14 -0800

    testing seeding for render

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -19,8 +19,9 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_products()
-        undo_categories()
+        
+        # undo_products()
+        # undo_categories()
         undo_pets()
         undo_users()
     seed_users()
